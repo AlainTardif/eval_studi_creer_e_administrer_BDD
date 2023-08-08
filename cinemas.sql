fe93dbfa4ebfbfb3b07863908154fbf92541bb26
Alain Tardif <tardif.alain@gmail.com>
2023-08-08 11:11:54 +0200

    création de l'admin et du user

--- cinemas.sql
+++ cinemas.sql
@@ -8,3 +8,14 @@ SHOW databases;
 
 -- je crée la BDD cinemas
 CREATE DATABASE IF NOT EXISTS cinemas CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- je me connecte à ma BDD cinemas avec la cde 
+USE cinemas;
+
+-- je crée un administrateur avec tous les droits
+CREATE USER 'admin3'@'localhost' IDENTIFIED BY 'kyxfJc4N3yqS';
+GRANT ALL PRIVILEGES ON * . * TO 'admin3'@'localhost';
+
+-- je crée un utilisateur avec vue sur les données uniquement
+CREATE USER 'user3'@'localhost' IDENTIFIED BY 'ZqBpw5mqs3Ge';
+GRANT SELECT ON * . * TO 'user3'@'localhost';
